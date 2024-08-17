cd6635a2af1d32cb7cfc7b8a2a4b989b5e005737
saimum <saimumrahman1214@gmail.com>
2024-08-17 23:47:01 +0600

    sds

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,33 @@
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
+using MediatR;
+
+namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Catagory, string Description, string ImageFile, decimal Price):ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            #region Create Product Entity from Command Object
+            var product = new Product
+            {
+                Name = command.Name,
+                Catagory = command.Catagory,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,  
+            };
+            #endregion
+
+            #region Save to  Database
+
+            #endregion
+
+            #region  Return CreateProductResult result          
+            return  new CreateProductResult(Guid.NewGuid());
+            #endregion
+        }
+    }
+}
---
+++ src/Services/Catalog/CatalogAPI/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+#region Add Service to the Container
+
+#endregion
+
+var app = builder.Build();
+
+#region Configure the HTTP request pipeline
+
+#endregion
+
+app.Run();
