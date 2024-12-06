74559f703dd13ed52fe2992348d059956476a409
saimum <saimumrahman1214@gmail.com>
2024-12-07 01:29:44 +0600

    Delete product done

---
+++ src/Services/Catalog/CatalogAPI/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,27 @@
+﻿
+using CatalogAPI.Products.UpdateProduct;
+
+namespace CatalogAPI.Products.DeleteProduct
+{
+    //public record DeleteProductRequest(Guid Id);
+    public record DeleteProductResponse(bool IsSuccess);
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id}", async (Guid Id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteProductCommand(Id));
+
+                var response = result.Adapt<DeleteProductResponse>();
+
+                return Results.Ok(response);
+            })
+                .WithName("Delete Product")
+                .Produces<DeleteProductResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Delete Product")
+                .WithDescription("Delete Products By Id");
+        }
+    }
+}
