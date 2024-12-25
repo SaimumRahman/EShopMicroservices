0d4e739a81bae243f753ab7493c8d9d7b9e57e93
saimum <saimumrahman1214@gmail.com>
2024-12-25 23:31:46 +0600

    globalk exception added in programs cs class

--- src/Services/Catalog/CatalogAPI/Program.cs
+++ src/Services/Catalog/CatalogAPI/Program.cs
@@ -1,4 +1,6 @@
 using BuildingBlocks.Behavior;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,4 +26,30 @@ var app = builder.Build();
 app.MapCarter();
 #endregion
 
+
+#region Exception Handler
+
+app.UseExceptionHandler(exceptionhandler => exceptionhandler.Run(async context =>
+{
+    var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+    if (exception == null) return;
+
+    var problemsDetals = new ProblemDetails()
+    {
+        Title = exception.Message,
+        Status = StatusCodes.Status500InternalServerError,
+        Detail = exception.StackTrace,
+    };
+
+    var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+    logger.LogError(exception,exception.Message);
+
+    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+    context.Response.ContentType = "application/problem+json";
+
+    await context.Response.WriteAsJsonAsync(problemsDetals);
+
+}));
+
+#endregion
 app.Run();
