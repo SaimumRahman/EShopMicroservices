cc7adfd23568e415c29d014e95006e4e746c1ec3
saimum <saimumrahman1214@gmail.com>
2024-12-27 20:44:25 +0600

    logger mediat r pipeline done

--- src/Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsHandler.cs
@@ -4,13 +4,11 @@ namespace CatalogAPI.Products.GetProducts
 {
     public record GetProductsQuery():IQuery<GetProductsResults>;
     public record GetProductsResults(IEnumerable<Product> Products);
-    internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+    internal class GetProductsQueryHandler(IDocumentSession session)
         : IQueryHandler<GetProductsQuery, GetProductsResults>
     {
         public async Task<GetProductsResults> Handle(GetProductsQuery query, CancellationToken cancellationToken)
         {
-            logger.LogInformation("GetProductsQueryHandler.Handle Called with {@Query}", query);
-
             var product = await session.Query<Product>().ToListAsync(cancellationToken); 
 
             return new GetProductsResults(product);
--- src/Services/Catalog/CatalogAPI/Program.cs
+++ src/Services/Catalog/CatalogAPI/Program.cs
@@ -1,6 +1,4 @@
 
-
-
 var builder = WebApplication.CreateBuilder(args);
 
 #region Add Service to the Container
@@ -10,6 +8,7 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
 builder.Services.AddValidatorsFromAssembly(assembly);
 builder.Services.AddCarter();
